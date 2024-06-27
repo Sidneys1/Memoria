b66b92495d0eaee24d5f05f29f4f3f24abc335ed
Sidneys1 <sidneys1@live.com>
2024-06-27 14:43:40 +0000

    Update settings page with new allow/denylists; refs #3

--- src/memoria/util.py
+++ src/memoria/util.py
@@ -4,6 +4,15 @@ from contextlib import AbstractAsyncContextManager, AbstractContextManager
 from contextvars import ContextVar, Token
 from typing import Generic, TypeVar
 
+
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
 T = TypeVar('T')
 
 
--- src/memoria/web/routes/settings.py
+++ src/memoria/web/routes/settings.py
@@ -4,8 +4,11 @@ from . import HX
 
 @APP.get('/settings/')
 @HX.page('settings.html.j2')
-async def settings() -> None:
-    ...
+async def settings():
+    from ...plugins._plugin_suite import PluginSuite
+    from ...plugins.allowlist import AllowlistRule
+    suite = PluginSuite()
+    return {'rule_plugin_docs': [(x.identifier, x.LONG_DOCUMENTATION, x.LONG_DOC_EXAMPLES) for x in suite._plugins.values() if issubclass(x, AllowlistRule) and x.LONG_DOCUMENTATION is not None]}
 
 
 __all__ = tuple()
